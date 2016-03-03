a82caccf11716d9f09491963074462a07b413efc
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-03-03 09:58:08 -0600

    got server running

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const app = require('express')()
+const server = require('http').createServer(app)
+// const io = require('socket.io')(server)
+
+const PORT = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+    res.send('heard dat!')
+})
+
+server.listen(PORT, () => {
+    console.log(`this here server listening on port: ${PORT}`)
+})
+
