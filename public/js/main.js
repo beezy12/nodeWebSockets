c3948c2b1f5db01c2a1741728180bbe2bbfa36bf
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-03-03 13:21:17 -0600

    chat app displays on two diff browser windows

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,55 @@
-// io.connect()
+;(function () {
+    'use strict'
+
+    const ws = io.connect()
+
+    ws.on('connect', () => {
+        console.log('socket connected')
+    })
+
+    ws.on('receiveChat', msg => {
+        console.log(msg)
+        displayChat(msg.name, msg.text)
+    })
+
+    // document.query selector - Returns the first element within the document that matches the specified group of selectors.
+    const form = document.querySelector('form')
+    const name = document.querySelector('input[name="name"]')
+    const text = document.querySelector('input[name="text"]')
+    const ul = document.querySelector('ul')
+
+    form.addEventListener('submit', () => {
+        const [n, t] = [name.value, text.value]
+        // now send this text over the socket on submit to the server or db
+        // so we'll set up something in the server to listen
+        // notice 'sendChat' matches the name in server.js listener
+        ws.emit('sendChat', {
+            name: n,
+            text: t
+    })
+
+    displayChat(n, t)
+
+    text.value = ''
+
+    event.preventDefault()
+    })
+
+    function displayChat (name, text) {
+        const li = generateLI(name, text)
+
+        ul.appendChild(li)
+    }
+
+    function generateLI (name, text) {
+        const li = document.createElement('li')
+        const textNode = document.createTextNode(`${name}: ${text}`)
+
+        li.appendChild(textNode)
+            return li
+    }
+
+}());
+
+
+
--- server.js
+++ server.js
@@ -22,5 +22,14 @@ server.listen(PORT, () => {
 
 // subscribe to events here. event is connection. a client is connecting to node
 ws.on('connection', socket => {
-    console.log('connection heeerrree*******', socket);
+    console.log('server.js connection heeerrree*******');
+
+    // here is where I am receiving the data sent by main.js
+    // once this server receives an event, it can save it to the db
+    socket.on('sendChat', (msg) => {
+        // console.log(msg)
+        // server has heard the message and is emitting it to everyone else over the server
+        // so now the client in main.js has to listen for this
+        socket.broadcast.emit('receiveChat', msg)
+    })
 })
